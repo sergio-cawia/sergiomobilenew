ca39bdf1cb8b8cb742c9da5e313ea94ae810369d
sergio-cawia <sergio@cawia.com>
2014-09-09 10:56:45 -0700

    a
    ad

---
+++ data.php
@@ -0,0 +1,30 @@
+<?
+
+if ( $_GET[ "job" ] == "header" )
+{
+	print "This is the header test";
+	exit;	
+}
+
+if ( $_GET[ "job" ] == "main" )
+{
+	print "This is the main content.";
+	exit;	
+}
+
+if ( $_GET[ "job" ] == "replacemain" )
+{
+	print "This is the replacement main content.This is the replacement main content.This is the replacement main content.This is the replacement main content.This is the replacement main content.This is the replacement main content.This is the replacement main content.HELLO.";
+	exit;	
+}
+
+if ( $_GET[ "job" ] == "footer" )
+{
+	print "<button onclick='replacemain()' data-theme='a'>Press me</button><br>This is the footer.";
+	exit;	
+}
+
+
+
+
+?>
